9202e2f1e75b7200c4bc32b18c43557ff46d9d55
Erin M <erin.michon@gmail.com>
2022-01-10 16:55:37 -0500

    lesson 14.2.5

--- server.js
+++ server.js
@@ -19,6 +19,23 @@ app.set('view engine', 'handlebars');
 // turn on routes
 app.use(routes);
 
+//Set up an express.js servier and connect session to our sequelize db
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
